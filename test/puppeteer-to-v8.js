327c1ef2a0caf62a702d139d0f13d60add213633
Viktor Köves <viktorkoves@gmail.com>
2018-02-24 17:29:03 -0600

    feat: implemented puppeteer to V8 class (#4)

--- test/puppeteer-to-v8.js
+++ test/puppeteer-to-v8.js
@@ -1,12 +1,22 @@
-/* globals describe, it */
+/* globals describe, it, before */
 
-const puppeteerToV8 = require('../lib/puppeteer-to-v8')()
+var PuppeteerToV8 = require('../lib/puppeteer-to-v8')()
 
 require('chai').should()
 
 describe('puppeteer-to-v8', () => {
+  let v8Coverage
+  const fixture = require('./fixtures/function-coverage-missing')
+
+  before(() => {
+    PuppeteerToV8.setCoverageInfo(fixture)
+
+    v8Coverage = PuppeteerToV8.convertCoverage()
+  })
+
   it('translates ranges into v8 format', () => {
-    console.info(puppeteerToV8)
+    // V8 coverage has ranges on a functions object, so check for that
+    v8Coverage[0].functions.ranges.should.eql(fixture[0].ranges)
   })
 
   // use mkdirp:
@@ -17,8 +27,9 @@ describe('puppeteer-to-v8', () => {
 
   // look at the uuid library:
   // uuid.v4()
-  it('generates scriptID', () => {
-
+  it('generates scriptId', () => {
+    // Ensures that the scriptId is of type 'number'
+    (typeof v8Coverage[0].scriptId).should.eql('number')
   })
 
   // for this test case, make sure we cover what happens

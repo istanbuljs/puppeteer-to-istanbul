ad9c2c109ac8c4d8db6fad10af1823ceef7e8cd7
Viktor Koves <viktorkoves@gmail.com>
2018-02-24 21:59:56 -0600

    Added fixture that is based off of two inline <script> tags
    Co-authored-by: Robert Altman <rjaltman04@gmail.com>

---
+++ test/sample_js/two-inline.js
@@ -0,0 +1,19 @@
+/*
+  These two blocks were included into the HTML used by the test runner
+  to create fixtures/two-inline.json
+ */
+
+// <script> block 1
+function c(num1, num2) {
+  return num2 * num1;
+}
+function d(num3) {
+  return num3;
+}
+c(4,3);
+
+//  <script> block 2
+function e(num4, num5, num6) {
+  return num4 * num5 - num6;
+}
+e(3,5,6);

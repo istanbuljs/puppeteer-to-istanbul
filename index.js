f7d94e0a0cee51468d872c1d293b3728b56d18be
Benjamin <ben@npmjs.com>
2018-02-25 09:58:07 -0800

    fix: address linting problems

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const PuppeteerToIstanbul = require('./lib/puppeteer-to-istanbul')
 
 module.exports = {
   write: (puppeteerFormat) => {
-    const PuppeteerToIstanbul = require('./lib/puppeteer-to-istanbul')
     const pti = PuppeteerToIstanbul(puppeteerFormat)
     pti.writeIstanbulFormat()
   }

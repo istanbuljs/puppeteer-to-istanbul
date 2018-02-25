17f9b7c9b03347657689926fb84a77fc215413ad
Robert J. Altman <rjaltman04@gmail.com>
2018-02-24 20:59:53 -0600

    Wrote skeleton of the output files

--- test/output-files.js
+++ test/output-files.js
@@ -1,24 +1,45 @@
 /* globals describe, it */
 
 const OutputFiles = require('../lib/output-files')
+const rimraf = require('rimraf')
+const fs = require('fs')
 
 require('chai').should()
 
 describe('puppeteer-to-v8', () => {
   describe('filename generation', () => {
-    // we can use path.basename(path[, ext]).
-    it('exposes a helper for generating a filename', () => {
-      const outputFiles = OutputFiles() // you would pass the JSON blob in here.
-      console.info(outputFiles.rewritePath('./foo/bar/hey.txt'))
+
+    beforeEach(() => {
+      rimraf.sync('./coverage');
     })
 
-    it('appropriately handles colliding names', () => {
+    // we can use path.basename(path[, ext]).
+    it('exposes a handler that appropriately handles colliding names', () => {
+      const outputFiles = OutputFiles(require('./fixtures/block-else-not-covered.json'))
+      console.log('Test');
+      var a = outputFiles.rewritePath('./sample_js/block-else-not-covered-1.js')
+      a.should.eql('./coverage/js/block-else-not-covered-1.js')
+    })
 
+    it('handle multiple files with same name, and replace in json', () => {
+      // Input from the fixture should be JSONified already
+      const fixture = require('./fixtures/function-coverage-full-duplicate.json')
+      const outputFiles = OutputFiles(fixture).coverageInfo
+      console.log(outputFiles)
+      outputFiles[0].url.should.eql(fixture[0].url)
+      outputFiles[1].url.should.eql(fixture[0].url
+        .replace('.js', '-1.js'))
     })
 
     // call it something like indexHTML-inline-1.js
     it('appropriately handles inline JavaScript', () => {
-
+      
     })
+
+    after(() => {
+      rimraf.sync('./coverage');
+    })
+
+
   })
 })

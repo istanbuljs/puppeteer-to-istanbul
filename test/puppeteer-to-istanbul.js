19c38954249074a31f85897719626e24c66dd2a2
Jonathan Santerre <jonathan@santerre.dev>
2019-11-04 18:42:54 -0500

    Add test to expect a valid out.json file
    Add a test to get 100% coverage
    
    only consider lib files in coverage
    
    cleanup test for valid nyc report

--- test/puppeteer-to-istanbul.js
+++ test/puppeteer-to-istanbul.js
@@ -1,19 +1,25 @@
-// /* globals describe, it, before */
+/* globals describe, it */
 
-// var PuppeteerToIstanbul = require('../lib/puppeteer-to-istanbul')()
+const should = require('chai').should()
+const fs = require('fs')
 
-// require('chai').should()
+var PuppeteerToIstanbul = require('../lib/puppeteer-to-istanbul')
 
-// describe('puppeteer-to-istanbul', () => {
+describe('puppeteer-to-istanbul', () => {
+  it('outputs a valid out.json file', () => {
+    const fixture = require('./fixtures/two-inline.json')
+    const pti = PuppeteerToIstanbul(fixture)
+    pti.writeIstanbulFormat()
+    const content = fs.readFileSync('.nyc_output/out.json', 'utf8')
+    const jsonObject = JSON.parse(content)
+    should.exist(jsonObject)
+    fs.unlinkSync('.nyc_output/out.json')
+  })
 
-//   const fixture = require('./fixtures/function-coverage-missing.json')
-
-//   before(() => {
-
-//   })
-
-//   it('translates ranges into lines for istanbul', () => {
-
-//   })
-
-// })
+  it('correctly sets coverage info', () => {
+    const fixture = require('./fixtures/two-inline.json')
+    const pti = PuppeteerToIstanbul(fixture)
+    pti.setCoverageInfo(fixture)
+    pti.coverageInfo.should.eql(fixture)
+  })
+})
